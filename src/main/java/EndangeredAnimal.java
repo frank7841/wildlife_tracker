09cef373b4dfec7acfc25c3a3079768a0ac72c59
frank7841 <franklinewax@gmail.com>
2020-07-14 09:40:00 +0300

    Created save method to insert values in to the animals table

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -3,24 +3,41 @@ import org.sql2o.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-public class EndangeredAnimal extends Animal{
+public class EndangeredAnimal extends Animal {
     private String health;
     private String age;
     public static final String DATABASE_TYPE = "endangered";
+
     public EndangeredAnimal(String name, String health, String age) {
         super(name);
         this.name = name;
-        this.health= health;
+        this.health = health;
         this.age = age;
-        type=DATABASE_TYPE;
+        type = DATABASE_TYPE;
     }
 
-    public String getHealth() { return health; }
+    public String getHealth() {
+        return health;
+    }
 
-    public String getAge() {return age;}
+    public String getAge() {
+        return age;
+    }
 
     public static String getDatabaseType() {
         return DATABASE_TYPE;
     }
 
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, health, age, type) VALUES (:name, :health, :age, :type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("health", this.health)
+                    .addParameter("age", this.age)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
 }
