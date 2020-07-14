3cc32aa5a488e637de09174dc813ad34a43bcbf3
frank7841 <franklinewax@gmail.com>
2020-07-14 06:28:29 +0300

    Added rule to connect to the database in class Test

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,5 +1,11 @@
 import junit.framework.TestCase;
+import org.junit.Rule;
 
-public class EndangeredAnimalTest extends TestCase {
+public class EndangeredAnimalTest extends{
+    public EndangeredAnimal setupEndangeredAnimal(){
+        return new EndangeredAnimal("panther","ill","old");
+    }
+    @Rule
+    public DatabaseRule database=new DatabaseRule();
 
 }
