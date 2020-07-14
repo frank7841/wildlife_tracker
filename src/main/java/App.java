27561ac61ed0b1b2fd28fe39094874a51d4c4575
frank7841 <franklinewax@gmail.com>
2020-07-14 15:02:21 +0300

    created route to home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,25 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
 public class App {
-    public static int main(String[] args){
-        return 0;
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {//show homw page
+            Map<String, Object> model = new HashMap<>();
+            List<EndangeredAnimal> animals = EndangeredAnimal.all();
+            model.put("animals",animals);
+            model.put("sightings",Sightings.all());
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
