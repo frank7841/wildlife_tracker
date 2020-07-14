ca7bb47b13abe9c577037c787b10d898ff4a3bd6
frank7841 <franklinewax@gmail.com>
2020-07-14 11:11:36 +0300

    Created and Called Database Rule

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,2 +1,11 @@
+
 public class Sightings {
+    public int animalId;
+    private String location;
+    private String rangerName;
+    private int id;
+
+    public  Sightings(int animalId, String location, String rangerName){
+
+    }
 }
--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,5 +1,10 @@
 import junit.framework.TestCase;
+import org.junit.Rule;
 
-public class SightingsTest extends TestCase {
+public class SightingsTest  {
+    public Sightings setupSighting(){ return new Sightings(1,"Riverbank","Mbaku"); }
+    @Rule
+    public DatabaseRule database=new DatabaseRule();
+
 
 }
