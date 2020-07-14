3931aec257e2166c932a90db310cdfddfaf3e9a6
frank7841 <franklinewax@gmail.com>
2020-07-14 11:21:16 +0300

    Created getLocation method

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -6,6 +6,12 @@ public class Sightings {
     private int id;
 
     public  Sightings(int animalId, String location, String rangerName){
+        this.animalId = animalId;
+        this.location = location;
+        this.rangerName =rangerName;
 
     }
+
+    public String getLocation() { return location; }
+
 }
