66684ac61aff42918b3cdf2f9e7cfa6a28d9756d
frank7841 <franklinewax@gmail.com>
2020-07-14 11:15:01 +0300

    created test to check whether Sightings instanciates properly

--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,10 +1,17 @@
 import junit.framework.TestCase;
 import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SightingsTest  {
     public Sightings setupSighting(){ return new Sightings(1,"Riverbank","Mbaku"); }
     @Rule
     public DatabaseRule database=new DatabaseRule();
 
-
+    @Test
+    public void Sighting_instanciatesCorrectly_true(){
+        Sightings testSighting=setupSighting();
+        assertEquals(true,testSighting instanceof Sightings);
+    }
 }
