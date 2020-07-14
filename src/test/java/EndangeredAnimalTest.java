b56a0cd604826df8aed042e651a5e1df48564930
frank7841 <franklinewax@gmail.com>
2020-07-14 06:31:05 +0300

    Ctreated test that checks if   endangered animals correctly instanciated

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,11 +1,21 @@
 import junit.framework.TestCase;
 import org.junit.Rule;
+import org.junit.Test;
 
-public class EndangeredAnimalTest extends{
-    public EndangeredAnimal setupEndangeredAnimal(){
-        return new EndangeredAnimal("panther","ill","old");
+import static org.junit.Assert.assertEquals;
+
+public class EndangeredAnimalTest  {
+    public EndangeredAnimal setupEndangeredAnimal() {
+        return new EndangeredAnimal("panther", "ill", "old");
     }
-    @Rule
-    public DatabaseRule database=new DatabaseRule();
 
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void EndangeredAnimals_instanciatesCorrectly_true() {
+        EndangeredAnimal testEndangeredAnimal = setupEndangeredAnimal();
+        assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
+
+    }
 }
