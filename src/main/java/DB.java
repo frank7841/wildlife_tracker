0209576eb13858c3958d6c6bc837c82e28e031b0
frank7841 <franklinewax@gmail.com>
2020-07-14 19:07:30 +0300

    setting dataBase for diployment

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,28 @@
-import org.sql2o.Sql2o;
-
+import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "frankline", "myPassword");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "frankline" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "myPassword" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,9 +1,24 @@
 import org.junit.rules.ExternalResource;
-import org.sql2o.Sql2o;
+import org.sql2o.*;
 
 public class DatabaseRule extends ExternalResource {
+
     @Override
     protected void before() {
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "frankline", "myPassword");
     }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+//            String deleteJoinsQuery = "DELETE FROM communities_persons *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+//            con.createQuery(deleteJoinsQuery).executeUpdate();
+        }
+    }
+
+
 }
