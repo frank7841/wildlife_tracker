969dafd1fa6d611479f24ec4f01a6e3296ba090e
frank7841 <franklinewax@gmail.com>
2020-07-14 14:52:54 +0300

    implementing App calss

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -1,2 +1,15 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+    }
 }
